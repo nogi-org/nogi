e8fafa436529217ba4f922f76b9ef41495427fa6
won taek oh <dnjsxorchlrh@naver.com>
2025-01-23 23:35:43 +0900

    Initial commit

---
+++ src/main/java/kr/co/nogibackend/NogiBackendApplication.java
@@ -0,0 +1,13 @@
+package kr.co.nogibackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NogiBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NogiBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/kr/co/nogibackend/NogiBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.co.nogibackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NogiBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
