3f0185521497785d8d2ff68bbc34e1e6f4bb2c5f
superpil0220 <dev.superpil@gmail.com>
2025-02-10 22:19:01 +0900

    feat: 알림을 위한 에러 핸들링

--- src/main/java/kr/co/nogibackend/domain/notion/dto/result/NotionEndTILResult.java
+++ src/main/java/kr/co/nogibackend/domain/notion/dto/result/NotionEndTILResult.java
@@ -1,10 +1,6 @@
 package kr.co.nogibackend.domain.notion.dto.result;
 
-// public record NotionEndTILResult(
-// 	Long userId,// 유저 ID
-// 	String notionAuthToken,// notion auth token
-// 	String notionPageId,// 노션 페이지 ID
-// 	String category,// 디렉터리 하위 구조
-// 	String title// 제목(ex 파일명.md 에서 파일명으로 사용할 값)
-// ) {
-// }
+public record NotionEndTILResult(
+	String notionPageId// 노션 페이지 ID
+) {
+}
