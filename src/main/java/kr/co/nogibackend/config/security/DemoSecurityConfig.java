c651d4728edb73c3f3de848d3803a5dbba821446
superpil0220 <dev.superpil@gmail.com>
2025-02-12 22:06:41 +0900

    chore: cors 임시설정

---
+++ src/main/java/kr/co/nogibackend/config/security/DemoSecurityConfig.java
@@ -0,0 +1,33 @@
+package kr.co.nogibackend.config.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+/*
+  Package Name : kr.co.nogibackend.config.security
+  File Name    : DemoSecurityConfig
+  Author       : superpil
+  Created Date : 25. 2. 12.
+  Description  :
+ */
+@Configuration
+public class DemoSecurityConfig {
+
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry
+					.addMapping("/**")
+					.allowedOrigins("http://localhost:5173")
+					.allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+					.allowedHeaders("*")
+					.allowCredentials(true);
+			}
+		};
+	}
+
+}
