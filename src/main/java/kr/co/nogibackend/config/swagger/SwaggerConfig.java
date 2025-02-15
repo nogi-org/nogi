186922fc644936c4cce020365f24f2ad73a9a4a7
won taek oh <dnjsxorchlrh@naver.com>
2025-02-15 20:12:55 +0900

    build: Swagger 추가

---
+++ src/main/java/kr/co/nogibackend/config/swagger/SwaggerConfig.java
@@ -0,0 +1,32 @@
+package kr.co.nogibackend.config.swagger;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import io.swagger.v3.oas.models.security.SecurityScheme.In;
+
+@Configuration
+public class SwaggerConfig {
+
+	@Bean
+	public OpenAPI openAPI() {
+		SecurityRequirement securityRequirement = new SecurityRequirement().addList("JWT-Cookie");
+
+		Components components = new Components()
+			.addSecuritySchemes(
+				"JWT-Cookie",
+				new SecurityScheme()
+					.name("ac_token") // 쿠키 이름과 동일하게 설정
+					.type(SecurityScheme.Type.APIKEY) // API Key 방식 사용
+					.in(In.COOKIE) // 쿠키에서 읽도록 설정
+			);
+
+		return new OpenAPI()
+			.addSecurityItem(securityRequirement)
+			.components(components);
+	}
+}
