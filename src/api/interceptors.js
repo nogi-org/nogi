2a9425c4fc2d9a84533331cd8794968dbe7148bf
superpil0220 <dev.superpil@gmail.com>
2025-02-20 21:04:14 +0900

    feat: 401, 403 모달차

--- src/api/interceptors.js
+++ src/api/interceptors.js
@@ -5,14 +5,15 @@ import {
 } from '@/api/apiResponse.js';
 import router from '@/router/index.js';
 import { useAuthStore } from '@/stores/authStore.js';
+import { useApiResponseModalStore } from '@/stores/apiResponseModalStore.js';
 
 export function setInterceptors(instance) {
   //요청 인터셉터
   instance.interceptors.request.use(
-    (config) => {
+    config => {
       return config;
     },
-    (error) => {
+    error => {
       //요청 에러 시 수행 로직
       return Promise.reject(error);
     }
@@ -20,10 +21,10 @@ export function setInterceptors(instance) {
 
   //응답 인터셉터
   instance.interceptors.response.use(
-    (response) => {
+    response => {
       return convertResponseFormat(response.data);
     },
-    (error) => {
+    error => {
       const response = convertResponseFormat(error.response.data);
       handleInterceptorCommonError(response);
       return Promise.reject(response);
@@ -37,13 +38,17 @@ function handleInterceptorCommonError(response) {
   const isCommonError = handleCommonError(response);
   if (!isCommonError) return;
 
+  const apiResponseModalStore = useApiResponseModalStore();
+
   switch (response.code) {
     case ApiResponse.USER_2: // 401
       router.push({ name: 'home' });
       useAuthStore().deleteAuth();
+      apiResponseModalStore.onActive(response);
       break;
     case ApiResponse.USER_3: // 403
       router.push({ name: 'home' });
+      apiResponseModalStore.onActive(response);
       break;
   }
 }
