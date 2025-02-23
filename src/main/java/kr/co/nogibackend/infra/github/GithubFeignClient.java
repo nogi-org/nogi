901c84924939624f845ec6af8aa5f382ac2d8093
won taek oh <dnjsxorchlrh@naver.com>
2025-02-24 00:02:32 +0900

    feat: 레포지토리명 수정시 "새롭게 생성" -> "레포지토리명 수정" 되도록 로직 수정

--- src/main/java/kr/co/nogibackend/infra/github/GithubFeignClient.java
+++ src/main/java/kr/co/nogibackend/infra/github/GithubFeignClient.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
@@ -28,6 +29,7 @@ import kr.co.nogibackend.domain.github.dto.request.GithubCreateIssueRequest;
 import kr.co.nogibackend.domain.github.dto.request.GithubCreateTreeRequest;
 import kr.co.nogibackend.domain.github.dto.request.GithubRepoRequest;
 import kr.co.nogibackend.domain.github.dto.request.GithubUpdateReferenceRequest;
+import kr.co.nogibackend.domain.github.dto.request.GithubUpdateRepoRequest;
 
 /*
   Package Name : kr.co.nogibackend.infra.github
@@ -71,6 +73,14 @@ public interface GithubFeignClient {
 	@PostMapping("/user/repos")
 	GithubRepoInfo createUserRepository(
 		@RequestBody GithubRepoRequest request,
+		@RequestHeader("Authorization") String token
+	);
+
+	@PatchMapping(value = "/repos/{owner}/{repo}")
+	GithubRepoInfo updateRepository(
+		@PathVariable("owner") String owner,
+		@PathVariable("repo") String repo,
+		@RequestBody GithubUpdateRepoRequest request,
 		@RequestHeader("Authorization") String token
 	);
 
