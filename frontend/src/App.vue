643b241eb85bdc524d22ef7c33baf287ff6c827f
superpil0220 <dev.superpil@gmail.com>
2025-02-28 20:08:03 +0900

    chore: 시스템점검On

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,7 +1,7 @@
 <script setup>
 import { RouterView } from 'vue-router';
 import SystemChecking from '@/components/common/SystemChecking.vue';
-const SYSTEM_CHECKING = false;
+const SYSTEM_CHECKING = true;
 </script>
 
 <template>
